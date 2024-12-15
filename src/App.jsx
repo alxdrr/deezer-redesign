8c33ec363883cda0419fe53e2fcbb839cf965177
alxdrr <lexlex277@gmail.com>
2024-12-15 16:19:02 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "./components/Sidebar";
+const App = () => {
+  return (
+    <div className="">
+      <p className="text-yellow-700 bg-black font-extrabold">Hai</p>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const sidebar = () => {
+  return <div className="text-red-500">HELLO FROM SIDEBAR</div>;
+};
+
+export default sidebar;
