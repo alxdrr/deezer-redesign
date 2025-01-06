2926074d6dd05f0b076902926719eec0a96b9010
alxdrr <lexlex277@gmail.com>
2025-01-06 15:34:08 +0800

    feat : add song info

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import Avatar from "../assets/album-cover/Album6.jpg";
+import { PlayerContext } from "../context/PlayerContext";
 import { motion } from "framer-motion";
+import verified from "../assets/icon/Information-verified.svg";
 
 const Information = ({ isOpen }) => {
+  const { track } = useContext(PlayerContext);
   const drawerVariants = {
     open: { x: 0, opacity: 1 },
     closed: { x: "100%", opacity: 0 },
@@ -16,56 +19,59 @@ const Information = ({ isOpen }) => {
       transition={{ type: "tween", duration: 0.2 }}
     >
       <div className="flex relative flex-col bg-white rounded-lg w-full gap-2 px-3 py-3 shadow-2 transition-colors duration-150 cursor-pointer items-center">
-        <img
-          src={Avatar}
-          alt="Cover"
-          className="w-full rounded-lg z-10 transition-opacity duration-300"
-        />
+        <div className="relative w-full h-auto rounded-lg">
+          <img src={track.image} alt="Cover" className="z-10 rounded-lg" />
+          <div className="absolute rounded-lg inset-0 bg-gradient-to-t from-black to-transparent opacity-70"></div>
+        </div>
+
         <div className="flex flex-col bottom-6 absolute left-6 w-auto">
-          <h1 className="text-sm text-neutral-0 z-20 font-bold">About You</h1>
-          <p className="text-[10px] z-20 w-auto text-white">The 1975</p>
+          <h1 className="text-sm text-neutral-0 z-20 font-bold">
+            {track.name}
+          </h1>
+          <p className="text-[10px] z-20 w-auto text-white">{track.artist}</p>
         </div>
       </div>
       <div className="flex relative flex-col bg-white rounded-lg w-full gap-2 px-3 py-3 shadow-2 transition-colors duration-150 cursor-pointer">
         <h1 className="text-3xl text-neutral-800 font-black">Artist</h1>
         <div className="relative">
-          <img
-            src={Avatar}
-            alt="Cover"
-            className="w-full max-h-24 rounded-lg z-10 transition-opacity duration-300"
-          />
+          <div className="relative w-full h-auto rounded-lg">
+            <img
+              src={track.image}
+              alt="Cover"
+              className="w-full max-h-24 object-cover rounded-lg z-10 transition-opacity duration-300"
+            />
+            <div className="absolute rounded-lg inset-0 bg-gradient-to-t from-transparent to-black opacity-90"></div>
+          </div>
+
           <div className="flex flex-col top-3 absolute left-3 w-auto">
-            <h1 className="text-sm text-neutral-0 z-20 font-bold">The 1975</h1>
+            <div className="flex gap-2">
+              <img src={verified} alt="" className="w-4" />
+              <h1 className="text-sm text-neutral-0 z-20 font-bold">
+                {track.artist}
+              </h1>
+            </div>
+
             <p className="text-[10px] z-20 w-auto text-white">
-              19,324,232 Monthly Listener
+              {track.listener} Monthly Listener
             </p>
           </div>
-          <p className="text-neutral-600 text-xs pt-2">
-            The 1975 is an English pop-rock band known for their genre-blending
-            sound and thought-provoking lyrics. Formed in 2002, the band
-            features Matty Healy (vocals), Adam Hann (guitar), Ross MacDonald
-            (bass), and George Daniel (drums).
-          </p>
+          <p className="text-neutral-600 text-xs pt-2">{track.artistProfile}</p>
         </div>
       </div>
       <div className="flex relative flex-col bg-white rounded-lg w-full gap-2 px-3 py-3 shadow-2 transition-colors duration-150 cursor-pointer">
         <h1 className="text-3xl text-neutral-800 font-black">Story</h1>
         <div className="relative">
-          <img
-            src={Avatar}
-            alt="Cover"
-            className="w-full rounded-lg z-10 transition-opacity duration-300"
-          />
+          <div className="relative w-full h-auto rounded-lg">
+            <img
+              src={track.image}
+              alt="Cover"
+              className="w-full rounded-lg z-10 transition-opacity duration-300"
+            />
+            <div className="absolute rounded-lg inset-0 bg-black/50"></div>
+          </div>
+
           <div className="flex flex-col top-1/2 left-1/2 absolute -translate-x-1/2 -translate-y-1/2 w-full text-center px-6 leading-5">
-            <p className="text-[10px] z-20 w-auto text-white">
-              "About You" by The 1975 is a hauntingly emotional track from the
-              band's fifth studio album, Being Funny in a Foreign Language. The
-              song features lush instrumentation, layered with reverb-drenched
-              guitars and ethereal synths, creating a dreamy yet melancholic
-              atmosphere. Matty Healy's poignant vocals narrate themes of love,
-              loss, and longing, as the lyrics explore the lingering emotions
-              tied to a past relationship.
-            </p>
+            <p className="text-[10px] z-20 w-auto text-white">{track.about}</p>
           </div>
         </div>
       </div>
