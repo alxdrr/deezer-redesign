8bbb4467ff53feb5c1844e038cfe3b1453a54ff3
alxdrr <lexlex277@gmail.com>
2025-01-12 04:04:33 +0800

    feat : Navbar logic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import logo from "../assets/image/LogoBlack.png";
 import Button from "../components/Button";
 const Navbar = () => {
+  const location = useLocation();
   return (
     <section id="navbar">
       <div className="fixed w-screen bg-white backdrop-blur-sm mx-auto px-0 md:px-4 lg:px-32 py-4 border-b-2 border-based-1 z-20">
@@ -10,18 +11,29 @@ const Navbar = () => {
           <Link className="z-10" to={"/"}>
             <img className="h-10" src={logo} alt="" />
           </Link>
-          <div className="flex justify-between gap-14  text-neutral-800">
-            <Link className="z-10 hover:text-primary" to={"/"}>
+          <div
+            className={`flex justify-between gap-14  text-neutral-800 ${
+              location.pathname === "/" ? "" : "hidden"
+            }`}
+          >
+            <Link
+              className={`z-10 hover:text-primary/50 ${
+                location.pathname === "/" ? "text-primary" : ""
+              }`}
+              to={"/"}
+            >
               Plans
             </Link>
             <Link className="z-10 hover:text-primary" to={"/explore"}>
               Features
             </Link>
-            <Link className="z-10 hover:text-primary" to={"/about-us"}>
+            <Link className="z-10 hover:text-primary" to={"/home"}>
               Music
             </Link>
           </div>
-          <div className="flex gap-2">
+          <div
+            className={`flex gap-2 ${location.pathname != "/" ? "hidden" : ""}`}
+          >
             <Button link={"/login"} title={"Log in"}></Button>
             <Button link={"/register"} title={"Sign up"}></Button>
           </div>
