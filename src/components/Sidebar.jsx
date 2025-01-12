43319c12b21d248f109d59fc923c28bcb624f18b
alxdrr <lexlex277@gmail.com>
2025-01-12 20:59:12 +0800

    feat : sidebar page active

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,63 +2,52 @@ import React from "react";
 import Button from "./Button";
 import MiniPlaylist from "./MiniPlaylist";
 import Logo from "../assets/image/LogoBlack.png";
-import IconHome from "../assets/icon/sidebar-home.svg";
-import IconExplore from "../assets/icon/sidebar-explore.svg";
+import { Link, useLocation } from "react-router";
 import { IoSearch } from "react-icons/io5";
 import { BiHomeAlt2, BiCollection } from "react-icons/bi";
-import IconCollection from "../assets/icon/sidebar-collection.svg";
-const sidebar = () => {
+const Sidebar = () => {
+  const location = useLocation();
   return (
     <div className="flex flex-col w-1/3 max-w-xs px-6 py-8 h-full border-r-2">
       <div className="w-full flex flex-col gap-4 h-auto">
         <img src={Logo} alt="Logo" className="max-w-32" />
         <ul>
-          <li className="flex gap-4 items-center text-primary mb-4 cursor-pointer">
-            <BiHomeAlt2 />
-            <p className="font-bold">Home</p>
-          </li>
-          <li className="flex items-center gap-4 text-neutral-500 transition-colors duration-150 mb-4 hover:text-primary cursor-pointer">
-            <IoSearch />
-            <p className="font-bold">Explore</p>
-          </li>
-          <li className="flex gap-4 items-center text-neutral-500 transition-colors duration-150 hover:text-primary cursor-pointer">
-            <BiCollection />
-            <p className="font-bold">Collection</p>
-          </li>
+          <Link to={"/home"}>
+            <li className={`flex gap-4 items-center mb-4 hover:text-primary duration-100 cursor-pointer ${location.pathname === "/home" ? "text-primary" : "text-neutral-500"}`}>
+              <BiHomeAlt2 />
+              <p className="font-bold">Home</p>
+            </li>
+          </Link>
+          <Link to={"/explore"}>
+            <li className={`flex gap-4 items-center mb-4 hover:text-primary duration-100 cursor-pointer ${location.pathname === "/explore" ? "text-primary" : "text-neutral-500"}`}>
+              <IoSearch />
+              <p className="font-bold">Explore</p>
+            </li>
+          </Link>
+          <Link to={"/collection"}>
+            <li
+              className={`flex gap-4 items-center mb-4 hover:text-primary duration-100 cursor-pointer ${location.pathname === "/colleciton" ? "text-primary" : "text-neutral-500"}`}
+            >
+              <BiCollection />
+              <p className="font-bold">Collection</p>
+            </li>
+          </Link>
         </ul>
         <div className="w-full flex flex-col px-4 py-5 text-white bg-primary rounded-lg gap-4">
-          <p className="text-xs">
-            You’re on Deezer Free. Upgrade & Feel the premium benefit for 1
-            month free
-          </p>
-          <Button
-            variant={"outline"}
-            type={"clickable"}
-            title={"Subscirbe"}
-          ></Button>
+          <p className="text-xs">You’re on Deezer Free. Upgrade & Feel the premium benefit for 1 month free</p>
+          <Button variant={"outline"} type={"clickable"} title={"Subscirbe"}></Button>
         </div>
       </div>
       <div id="container" className="flex flex-col gap-2 py-2 grow">
-        <Button
-          variant={"outline"}
-          type={"clickable"}
-          title={"New Playlist"}
-        ></Button>
-        <div
-          id="playlist"
-          className="flex flex-col gap-4 overflow-y-auto h-0 grow"
-        >
-          <MiniPlaylist></MiniPlaylist>
-          <MiniPlaylist></MiniPlaylist>
-          <MiniPlaylist></MiniPlaylist>
-          <MiniPlaylist></MiniPlaylist>
-          <MiniPlaylist></MiniPlaylist>
-          <MiniPlaylist></MiniPlaylist>
-          <MiniPlaylist></MiniPlaylist>
+        <Button variant={"outline"} type={"clickable"} title={"New Playlist"}></Button>
+        <div id="playlist" className="flex flex-col gap-4 overflow-y-auto h-0 grow">
+          <Link to={"/playlist"}>
+            <MiniPlaylist />
+          </Link>
         </div>
       </div>
     </div>
   );
 };
 
-export default sidebar;
+export default Sidebar;
--- src/pages/Plans.jsx
+++ src/pages/Plans.jsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion";
 import React from "react";
-import { Link } from "react-router";
 import Button from "../components/Button";
 import Navbar from "../components/Navbar";
 import Accordion from "../components/Accordion";
@@ -10,23 +9,13 @@ const plans = [
     name: "INDIVIDUAL",
     price: "$11.99",
     account: 1,
-    feature: [
-      "Ad-free",
-      "Download music",
-      "High Fidelity Sound",
-      "Unlimited and on demand",
-    ],
+    feature: ["Ad-free", "Download music", "High Fidelity Sound", "Unlimited and on demand"],
   },
   {
     name: "FAMILY",
     price: "$19.99",
     account: 6,
-    feature: [
-      "Ad-free",
-      "Download music",
-      "High Fidelity Sound",
-      "Unlimited and on demand",
-    ],
+    feature: ["Ad-free", "Download music", "High Fidelity Sound", "Unlimited and on demand"],
   },
 ];
 const featureStyle = (feature) => {
@@ -56,29 +45,15 @@ const card = (index, name, account, feature, price) => {
         className="drop-shadow-md w-96 bg-white rounded-xl px-6 py-6 flex flex-col justify-between items-center gap-4"
       >
         <div className="flex w-full justify-between gap-10 items-center">
-          <h1
-            className={`${
-              name == "INDIVIDUAL" ? "text-primary" : "text-[#FF673D]"
-            } text-xl xl:text-3xl lg:text-2xl text-secondary text-center font-black`}
-          >
-            {name}
-          </h1>
-          <p className="text-sm  text-gray-700 text-center">
-            {account} account
-          </p>
+          <h1 className={`${name == "INDIVIDUAL" ? "text-primary" : "text-[#FF673D]"} text-xl xl:text-3xl lg:text-2xl text-secondary text-center font-black`}>{name}</h1>
+          <p className="text-sm  text-gray-700 text-center">{account} account</p>
         </div>
         <div className="gap-0 w-full">
           <p className="text-neutral-800 font-medium">1 month free</p>
           <p className="text-neutral-500 w-auto">then {price}/month</p>
         </div>
-        <div className="flex w-full flex-col gap-3">
-          {feature.map((desc, index) => featureStyle(desc))}
-        </div>
-        <Button
-          variant={"primary"}
-          type={"clickable"}
-          title={"Try for free"}
-        ></Button>
+        <div className="flex w-full flex-col gap-3">{feature.map((desc, index) => featureStyle(desc))}</div>
+        <Button variant={"primary"} type={"clickable"} title={"Try for free"}></Button>
       </motion.div>
     </>
   );
@@ -98,27 +73,16 @@ const Plans = () => {
           className="w-full max-w-screen-xl flex flex-col justify-self-center my-auto mx-28 pt-16 gap-8 xl:gap-10 px-4 lg:px-8 xl:px-0"
         >
           <div>
-            <div className="text-neutral-800 text-3xl md:text-6xl lg:text-7xl font-black py-4 w-full">
-              Get 1 month of Premium
-            </div>
-            <div className="text-primary text-3xl md:text-6xl lg:text-7xl font-black py-4">
-              For Free
-            </div>
+            <div className="text-neutral-800 text-3xl md:text-6xl lg:text-7xl font-black py-4 w-full">Get 1 month of Premium</div>
+            <div className="text-primary text-3xl md:text-6xl lg:text-7xl font-black py-4">For Free</div>
             <div className="flex flex-col gap-6 w-auto">
-              <p className="text-neutral-500 w-auto">
-                We will remind you 7 days before your trial ends.
-              </p>
-              <Button
-                variant={"primary"}
-                type={"clickable"}
-                title={"Subscirbe"}
-              ></Button>
+              <p className="text-neutral-500 w-auto">We will remind you 7 days before your trial ends.</p>
+              <Button variant={"primary"} type={"clickable"} title={"Subscirbe"}></Button>
             </div>
           </div>
           <div className="flex flex-col gap-8 xl:gap-10">
             <div className="text-white lg:text-xl font-normal xl:max-w-xl">
-              Innovative Software House to Bring Your Ideas to Life. We are your
-              trusted partner in web, mobile, and software development.
+              Innovative Software House to Bring Your Ideas to Life. We are your trusted partner in web, mobile, and software development.
             </div>
           </div>
         </motion.div>
@@ -136,11 +100,7 @@ const Plans = () => {
           <p>Find The Perfect Plan</p>
           <p>Match Your Style!</p>
         </div>
-        <section className="flex gap-6 sm:flex-row w-full justify-center">
-          {plans.map((plan, index) =>
-            card(index, plan.name, plan.account, plan.feature, plan.price)
-          )}
-        </section>
+        <section className="flex gap-6 sm:flex-row w-full justify-center">{plans.map((plan, index) => card(index, plan.name, plan.account, plan.feature, plan.price))}</section>
       </motion.div>
       {/* Services Section */}
 
@@ -153,12 +113,8 @@ const Plans = () => {
         className="w-full max-w-screen-xl items-center flex flex-col h-auto my-16 mx-auto lg:py-10 px-4 gap-8 xl:gap-10 lg:px-8 xl:px-0"
       >
         <section className="flex justify-center items-center flex-col gap-4">
-          <h1 className="text-primary text-xl md:text-start md:text-2xl lg:text-4xl text-center font-black">
-            FAQ
-          </h1>
-          <p className="lg:text-base xl:text-xl max-w-4xl text-center text-gray-700">
-            Find answers to common questions about our services and processes.
-          </p>
+          <h1 className="text-primary text-xl md:text-start md:text-2xl lg:text-4xl text-center font-black">FAQ</h1>
+          <p className="lg:text-base xl:text-xl max-w-4xl text-center text-gray-700">Find answers to common questions about our services and processes.</p>
         </section>
         <div className="flex flex-col justify-center gap-3 items-center w-full">
           <Accordion
