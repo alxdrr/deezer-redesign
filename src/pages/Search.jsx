e5c446fe2b5d1d826b63689cdb5a510f75ead3ab
alxdrr <lexlex277@gmail.com>
2025-01-14 21:01:57 +0800

    fix : search result style

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import axios from "axios";
+import search from "../assets/icon/search.gif";
+import { PlayerContext } from "../context/PlayerContext";
+import { IoPlayCircleSharp, IoPauseCircleSharp } from "react-icons/io5";
 // import AlbumCard from "../components/AlbumCard";
 const Search = ({ results, token }) => {
   const [trackData, setTrackData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-
+  const { playWithId, playStatus, track, pause } = useContext(PlayerContext);
   const fetchTrackDetails = async () => {
     if (!Array.isArray(results) || results.length === 0) {
       setError("No results to fetch");
@@ -58,64 +61,56 @@ const Search = ({ results, token }) => {
   }, [results]);
 
   return (
-    <div className={`w-full h-0 flex flex-col gap-8 overflow-y-auto grow px-16 py-4 ${results.length > 0 ? "" : "hidden"}`}>
-      <div className="h-auto gap-2.5 flex flex-col">
-        <p className="text-3xl text-neutral-800 font-black">Tracks</p>
-        <table className="table-fixed">
-          <thead className="border border-neutral-300 border-x-0 border-t-0">
-            <tr>
-              <th className="text-primary w-2/5 text-start font-normal py-3">TRACK</th>
-              <th className="text-primary w-1/5 text-start font-normal py-3">ARTIST</th>
-              <th className="text-primary w-1/5 text-start font-normal py-3">ALBUM</th>
-              <th className="text-primary w-1/5 text-start font-normal py-3">DURATION</th>
-            </tr>
-          </thead>
-          <tbody>
-            {trackData &&
-              trackData.map((result, index) => (
-                <tr key={index} className="w-full">
-                  <td className="text-neutral-800 w-auto flex items-center gap-4 py-2">
-                    <img src={result.album.images[0].url} alt="" className="w-12" />
-                    <p className="line-clamp-1">{result.name}</p>
-                  </td>
-                  {/* Jika result.artists adalah array, ambil nama artis pertama atau gabungkan nama artis */}
-                  <td className="text-neutral-800 w-1/5">{result.artists.map((artist) => artist.name).join(", ")}</td>
-                  <td className="text-neutral-800 w-1/5">{result.album.name}</td>
-                  <td className="text-neutral-800 w-1/5">{formatMilliseconds(result.duration_ms)}</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
+    <div className="w-full">
+      <div className={`w-full p-4 xl:p-10 flex flex-col gap-8 overflow-y-auto h-full ${results.length > 0 ? "" : "hidden"}`}>
+        <div className="h-auto gap-2.5 flex flex-col">
+          <p className="text-3xl text-neutral-800 font-black">Tracks</p>
+          <table className="table-fixed border-spacing-x-1 border-separate">
+            <thead className="border border-neutral-300 border-x-0 border-t-0">
+              <tr>
+                <th className="text-primary w-2/5 text-start font-normal py-3">TRACK</th>
+                <th className="text-primary w-1/5 text-start font-normal py-3">ARTIST</th>
+                <th className="text-primary w-1/5 text-start font-normal py-3">ALBUM</th>
+                <th className="text-primary w-1/5 text-start font-normal py-3">DURATION</th>
+              </tr>
+            </thead>
+            <tbody>
+              {trackData &&
+                trackData.map((result, index) => (
+                  <tr key={index} className="w-full hover:bg-purple-100 transition-colors duration-150 cursor-pointer">
+                    <td className="text-neutral-800 relative w-auto flex items-center gap-4 py-2 group ">
+                      <div className="absolute  aspect-square w-12 flex items-center justify-center h-full group-hover:opacity-100 group-hover:z-30 transition-opacity duration-300">
+                        <img src={result.album.images[0].url} alt="" className="brightness-50" />
+                        {playStatus && track.id === id ? (
+                          <>
+                            <IoPauseCircleSharp onClick={() => pause()} className="text-primary bg-white rounded-full text-2xl cursor-pointer absolute" />
+                          </>
+                        ) : (
+                          <IoPlayCircleSharp onClick={() => playWithId(id)} className="text-primary bg-white rounded-full text-2xl absolute cursor-pointer" />
+                        )}
+                      </div>
+                      <img src={result.album.images[0].url} alt="" className="w-12 z-0 group-hover:opacity-0 transition-opacity duration-300" />
+                      <p className="line-clamp-1">{result.name}</p>
+                    </td>
+                    {/* Jika result.artists adalah array, ambil nama artis pertama atau gabungkan nama artis */}
+                    <td className="text-neutral-800 w-1/5">
+                      <p className="line-clamp-1">{result.artists.map((artist) => artist.name).join(", ")}</p>
+                    </td>
+                    <td className="text-neutral-800 w-1/5">
+                      <p className="line-clamp-1">{result.album.name}</p>
+                    </td>
+                    <td className="text-neutral-800 w-1/5">{formatMilliseconds(result.duration_ms)}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
       </div>
 
-      {/* <div className="h-auto gap-2.5 flex flex-col">
-        <h1 className="text-3xl text-neutral-800 font-black">Albums</h1>
-        <div className="flex flex-wrap gap-4 h-auto">
-          {trackData &&
-            trackData.map((result, index) => (
-              <>
-                <AlbumCard key={index} name={result.name} artist={result.artists} cover={result.album.images[0].url} released={result.released_date} />
-              </>
-            ))}
-        </div>
-      </div> */}
-      {/* playlist */}
-      {/* <div className="h-auto gap-2.5 flex flex-col">
-        <h1 className="text-3xl text-neutral-800 font-black">Playlist</h1>
-        <div className="flex flex-wrap gap-4 h-auto">
-          {Array.isArray(results) && results.length > 0 && Array.isArray(results[3]) ? (
-            results[3].map((result, index) => (
-              <PlaylistCard key={index} name={result.name} owner={result.owner?.display_name} cover={result.images?.[0]?.url} total={result.tracks.total} />
-            ))
-          ) : (
-            <tr>
-              <td colSpan={4} className="text-center text-neutral-800">
-                No results found
-              </td>
-            </tr>
-          )}
-        </div>
-      </div> */}
+      <div className={`flex flex-col items-center top-1/2 left-1/2 absolute -translate-x-1/2 -translate-y-1/2 ${results.length > 0 ? "hidden" : ""}`}>
+        <img src={search} className="w-32" alt="" />
+        <p className="text-neutral-500 text-xl">Try search for some tracks</p>
+      </div>
     </div>
   );
 };
